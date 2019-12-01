ab8f9a14ce8bade4011e87d0d75124289c0cab80
AndrewLester <alester220@gmail.com>
2019-11-30 19:42:05 -0500

    Use FRCDashboard template and add camera

---
+++ src/connections.js
@@ -0,0 +1,29 @@
+const cameraLink1 = '10.14.18:1181';
+const cameraLink2 = '10.14.18:1182'
+
+const cameras = [
+  new Camera(document.getElementById('camera1'), cameraLink1, '../images/spinner.svg', '../images/error.svg'),
+  new Camera(document.getElementById('camera2'), cameraLink2, '../images/spinner.svg', '../images/error.svg')
+]
+
+NetworkTables.addRobotConnectionListener(onRobotConnection, true);
+// ipc.send('connect', '127.0.0.1');
+/**
+ * Function to be called when robot connects
+ * @param {boolean} connected
+ */
+function onRobotConnection(connected) {
+  var state = connected ? 'Robot connected!' : 'Robot disconnected.';
+  console.log(state);
+  loadCameras();
+}
+
+function loadCameras() {
+  for (let camera of cameras) {
+    camera.loadCameraStream().then((success) => {
+      if (success) {
+        console.log('Camera: ' + (cameras.indexOf(camera)) + ' loaded successfully');
+      }
+    });
+  }
+}
