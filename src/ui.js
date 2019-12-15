8b0639553202de721047411187ee1106815a522a
AndrewLester <alester220@gmail.com>
2019-12-15 13:31:42 -0500

    Save split settings in local storage

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,18 @@
 const Split = require('split.js');
 
+let sizes = localStorage.getItem('split-sizes');
+
+if (sizes) {
+    sizes = JSON.parse(sizes);
+} else {
+    sizes = [50, 50]; // default sizes
+}
+
 const split = Split(['#camera1', '#camera2'], {
-    sizes: [50, 50],
+    sizes: sizes,
     gutterAlign: 'center',
-    gutterSize: 5
+    gutterSize: 5,
+    onDragEnd: function(sizes) {
+         localStorage.setItem('split-sizes', JSON.stringify(sizes));
+    }
 });
