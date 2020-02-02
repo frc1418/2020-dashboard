979516cacf305840f609d767742faf4625337127
quirozj <57964624+quirozj@users.noreply.github.com>
2020-02-02 10:26:41 -0500

    Create windows-installer.js

---
+++ windows-installer.js
@@ -0,0 +1,12 @@
+const electronInstaller = require('electron-winstaller');
+
+electronInstaller.createWindowsInstaller({
+    appDirectory: '.',
+    outputDirectory: './distribution/windows-installer.exe',
+    authors: 'Team 1418',
+    exe: 'windows-installer.exe'
+}).then(() => {
+    console.log('Working!');
+}).catch((e) => {
+    console.log(`Error: ${e.message}`);
+});
