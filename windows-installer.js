ce4bfd487d44ad61d0b6f4f562d0ee1a5ea9eee2
AndrewLester <alester220@gmail.com>
2020-02-02 15:24:43 -0500

    Add build success output to js file

--- windows-installer.js
+++ windows-installer.js
@@ -1,12 +1,14 @@
 const electronInstaller = require('electron-winstaller');
 
 electronInstaller.createWindowsInstaller({
-    appDirectory: '/src',
+    appDirectory: `${__dirname}/src`,
     outputDirectory: '.',
     authors: 'Team 1418',
     exe: 'windows-installer.exe'
 }).then(() => {
     console.log('Working!');
+    process.exit(0);
 }).catch((e) => {
     console.log(`Error: ${e.message}`);
+    process.exit(1);
 });
